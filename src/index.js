e89a35ef218d857e59b3a3568ab6eb2cd3abd27e
Rishi Khanna <gs2013070@sgsgitsindore.in>
2023-02-28 11:00:18 +0530

    setup up the body-parser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const express=require('express');
-const {PORT} = require('./config/serverConfig')
+const bodyparser=require('body-parser');
+
+
+
+const {PORT} = require('./config/serverConfig.js');
+const { urlencoded } = require('body-parser');
+
 const setupAndStartServer=async function () {
     const app=express();
+
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended:true}));
     
     app.listen(PORT,function () {
         console.log(`server started with ${PORT}`);
